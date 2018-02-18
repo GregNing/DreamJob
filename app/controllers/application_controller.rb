80446259a6707cc7174f3a48750d98faab9fd512
Greg <GregJieNing@gmail.com>
2018-02-19 02:54:06 +0800

    singup view & is_admin sign_view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,10 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
-    add_flash_types :warning
-    before_action :configure_permitted_parameters, if: :devise_controller?
+    add_flash_types :warning    
    protected 
     def require_is_admin
         unless current_user.admin?
             redirect_to root_path,alert: "您尚未擁有管理者權限!"
         end            
-    end
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
     end
 end
