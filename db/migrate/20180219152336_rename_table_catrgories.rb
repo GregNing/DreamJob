3e2596f5a13894579a0c7a4855d345f435edfa98
Greg <GregJieNing@gmail.com>
2018-02-23 17:12:39 +0800

    rebuild migrate

--- db/migrate/20180219152336_rename_table_catrgories.rb
+++
@@ -1,5 +0,0 @@
-class RenameTableCatrgories < ActiveRecord::Migration[5.1]
-  def change    
-    # rename_table :categories, :category
-  end
-end
--- db/migrate/20180219152705_delete_table_catrgories.rb
+++
@@ -1,5 +0,0 @@
-class DeleteTableCatrgories < ActiveRecord::Migration[5.1]
-  def change
-    # drop_table :category
-  end
-end
