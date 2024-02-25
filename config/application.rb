2d29dde41a5b1c86d89917dbf9d246bcb65c3af6
Greg <GregJieNing@gmail.com>
2024-02-25 23:54:24 +0800

    use rails 7

--- config/application.rb
+++ config/application.rb
@@ -6,12 +6,12 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module Joblisting3
-  class Application < Rails::Application    
+  class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1	  
+    config.load_defaults 7.0
     # config.active_record.raise_in_transactional_callbacks = true
     # config.autoload_paths += %W(#{config.root}/app/uploaders)
-    
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
