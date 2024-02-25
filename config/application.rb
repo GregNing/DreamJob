328582f31a26256c6a3c2bef7b50844c5f67ebb7
Greg <GregJieNing@gmail.com>
2024-02-26 00:05:10 +0800

    fix zeitwerk issue

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Joblisting3
+module DreamJob
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
@@ -15,5 +15,6 @@ module Joblisting3
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.autoloader = :classic
   end
 end
